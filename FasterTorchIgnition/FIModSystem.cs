59eab00c8f5229d6663d3390e60ea60c25b672d0
brndd <burneddi@protonmail.com>
2025-01-26 22:35:00 +0200

    Load the mod on both client and server

--- FasterTorchIgnition/FIModSystem.cs
+++ FasterTorchIgnition/FIModSystem.cs
@@ -1,37 +1,23 @@
 ﻿using System;
 using HarmonyLib;
 using Vintagestory.API.Common;
-using Vintagestory.API.Server;
 
 namespace FasterTorchIgnition;
 
 public class FIModSystem : ModSystem
 {
-    public static ICoreServerAPI Api;
+    public static ICoreAPI Api;
     private Harmony _harmony;
-
-    public override bool ShouldLoad(EnumAppSide side)
+    
+    public override void Start(ICoreAPI api)
     {
-        return side == EnumAppSide.Server;
-    }
-
-    public override void StartServerSide(ICoreServerAPI api)
-    {
-        base.StartServerSide(api);
+        base.Start(api);
         Api = api;
 
         if (!Harmony.HasAnyPatches(Mod.Info.ModID))
         {
             _harmony = new Harmony(Mod.Info.ModID);
-            try
-            {
-                _harmony.PatchAll();
-            }
-            catch (Exception e)
-            {
-                api.Logger.Error($"Harmony exception: {e}");
-                throw;
-            }
+            _harmony.PatchAll();
         }
     }
 
